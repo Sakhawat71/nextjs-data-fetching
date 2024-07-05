3bb7e0826cb86aacca9eb436b70bf61b1b7cf742
sakhawat71 <sakhawat.showrav@gmail.com>
2024-07-05 20:32:27 +0600

    daisyUi

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const MainPage = () => {
+
+
+
     return (
-        <div>
-            <h1>Next app</h1>
+        <div className='font-serif'>
+            <h1 className='text-7xl text-green-500'>Next app</h1>
+            <p>For data fetching</p>
         </div>
     );
 };
